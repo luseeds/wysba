d9aaa1c7d4ae2022c914c526c16f87d385d4b09e
cyrilf <cyrilf7x@gmail.com>
2018-03-07 18:12:02 +0100

    fix(age): Fix the year plurality based on the age

--- src/components/age/Age.js
+++ src/components/age/Age.js
@@ -1,17 +1,23 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 
 import AgeForm from './AgeForm'
 import AgeInput from './AgeInput'
 
 import './Age.css'
 
-const Age = () => (
+const Age = ({ age }) => (
   <section className="age">
     <AgeForm>
-      <div className="age-question">I am <AgeInput/> years old</div>
+      <div className="age-question">I am <AgeInput/> {age > 1 ? 'years' : 'year'} old</div>
       { null && <button type="submit">Confirm</button> }
     </AgeForm>
   </section>
 )
+Age.propTypes = {
+  age: PropTypes.number,
+}
 
-export default Age
+const select = (state) => ({ age: state.age.value })
+export default connect(select)(Age)
