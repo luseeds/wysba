608476db856dc593a8c64c4656f174c73379fdac
cyrilf <cyrilf7x@gmail.com>
2018-03-07 15:59:16 +0100

    feat(app): Improve the App component structure

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 
-import logo from './logo.svg'
 import './App.css'
 import Age from 'components/age/Age'
 import Result from 'components/result/Result'
@@ -8,12 +7,11 @@ import Result from 'components/result/Result'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Would I still be alive?</h1>
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">Would I still be alive?</h1>
         </header>
-        <div className="App-intro">
+        <div className="app-core">
           <Age/>
           <Result/>
         </div>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
 import './index.css'
-import App from './App'
+import App from './components/app/App'
 import registerServiceWorker from './registerServiceWorker'
 import initializeStore from './store/initializeStore'
 
