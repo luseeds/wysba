4eb5282aef42aefa8d5179b66232f4792167a387
cyrilf <cyrilf7x@gmail.com>
2018-04-03 17:57:40 +0200

    fix(typo): Rename luseed to luseeds

--- src/components/share/Share.js
+++ src/components/share/Share.js
@@ -6,8 +6,8 @@ import './Share.css'
 const Share = () => (
   <Section title="Share" reverse className="result-share">
     <p>Feel free to share it. Education is super important nowadays that's why sharing knowledge is key.</p>
-    <div className="fb-share-button" data-href="http://wysba.luseeds.com" data-layout="button" data-size="large" data-mobile-iframe="true"><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwysba.luseeds.com%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a></div>
-    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-size="large" data-text="Farmed animals are killed at a very young age, if you were one, would you still be alive? Find the answer:" data-url="http://wysba.luseeds.com" data-via="luseeds" data-hashtags="wysba" data-dnt="true" data-show-count="false">Tweet</a>
+    <div className="fb-share-button" data-href="https://wysba.luseeds.com" data-layout="button" data-size="large" data-mobile-iframe="true"><a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwysba.luseeds.com%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a></div>
+    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-size="large" data-text="Farmed animals are killed at a very young age, if you were one, would you still be alive? Find the answer:" data-url="https://wysba.luseeds.com" data-via="luseeds" data-hashtags="wysba" data-dnt="true" data-show-count="false">Tweet</a>
   </Section>
 )
 
