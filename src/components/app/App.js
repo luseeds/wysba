1dfe3cff30480757ca8cfbff2e2ba47771999637
cyrilf <cyrilf7x@gmail.com>
2018-04-03 22:38:52 +0200

    feat(title): Add a description underneath the main title

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,6 +10,7 @@ class App extends Component {
       <div className="app">
         <header className="app-header">
           <h1 className="app-title">Would you still be alive?</h1>
+          <h2 className="app-description">Farmed animals are killed at a very young age. If you were one, would you still be alive? Find the answer.</h2>
         </header>
         <div className="app-core">
           <Age/>
