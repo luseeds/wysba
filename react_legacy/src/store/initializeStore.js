b0e3ca07ae8843d551b5ba090a0d12c75069af2f
cyrilf <1302282+cyrilf@users.noreply.github.com>
2025-03-26 22:07:45 +0100

    chore: Delete react_legacy

--- react_legacy/src/store/initializeStore.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux'
-import rootReducer from './rootReducer'
-
-export default (initialState) => createStore(
-  rootReducer,
-  initialState,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-)
