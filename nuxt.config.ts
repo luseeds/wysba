801d922389fbe6035f0827e6baf6be8288dcb893
cyrilf <1302282+cyrilf@users.noreply.github.com>
2025-03-26 11:49:35 +0100

    chore: Install and setup tailwind@4.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2025-03-27",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/eslint"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
